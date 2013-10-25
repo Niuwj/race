f25a894ea2b525cc63acc20715e14acba906e5a8
Niuwj <niuwj2011@gmail.com>
2013-10-25 20:38:33 +0800

    提升用户体验，实现确认信息 grails2.2.4

---
+++ grails-app/conf/DataSource.groovy
@@ -0,0 +1,44 @@
+dataSource {
+    pooled = true
+    driverClassName = "com.mysql.jdbc.Driver"
+    dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+    username = "root"
+    password = "root"
+}
+hibernate {
+    cache.use_second_level_cache = true
+    cache.use_query_cache = true
+    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
+}
+// environment specific settings
+environments {
+    development {
+        dataSource {
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:mysql://localhost/devdb"
+        }
+    }
+    test {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:mysql://localhost/testdb"
+        }
+    }
+    production {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:mysql://localhost/proddb"
+            pooled = true
+            properties {
+               maxActive = -1
+               minEvictableIdleTimeMillis=1800000
+               timeBetweenEvictionRunsMillis=1800000
+               numTestsPerEvictionRun=3
+               testOnBorrow=true
+               testWhileIdle=true
+               testOnReturn=true
+               validationQuery="SELECT 1"
+            }
+        }
+    }
+}
---
+++ web-app/js/racetrack.js
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: nwj
+ * Date: 13-10-25
+ * Time: 下午7:27
+ * To change this template use File | Settings | File Templates.
+ */
+function warnBeforeRaceDelete(){
+    return confim('确定删除？')
+}
